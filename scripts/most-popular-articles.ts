42b18937d4d8a303d26710d61af719eeca140229
alfredo.salzillo <noctisbels@gmai.com>
2020-11-15 14:36:14 +0100

    chore(most-popular-articles): based on section to the article

--- scripts/most-popular-articles.ts
+++ scripts/most-popular-articles.ts
@@ -1,3 +1,4 @@
+import { format } from 'https://deno.land/std/datetime/mod.ts';
 import { AsyncStream } from '../src/async-generators-helpers.ts';
 import { articles, ArticleSummary, createArticle } from '../src/articles.ts';
 import { readme } from "../src/readme.ts";
@@ -10,21 +11,27 @@ Automated with [alfredosalzillo/my-dev-to](https://github.com/alfredosalzillo/my
 
 {% github alfredosalzillo/my-dev-to %}
 `
-const today = () => {
-  const now = new Date();
-  return `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
+const elaboratedDay = () => {
+  const date = new Date();
+  date.setDate(date.getDate() - 1)
+  return format(date, 'yyyy-MM-dd');
 }
 const main = async () => {
   console.log("using env DEV_TO_PUBLISHED", Deno.env.get('DEV_TO_PUBLISHED'))
   const published = Deno.env.get('DEV_TO_PUBLISHED') === 'true';
   const allArticles = await AsyncStream.of(articles({ page: 1, top: 1 }, 1)).collect();
+  const totalOfArticles = allArticles.length;
+  const totalComments = allArticles.map((article) => article.comments_count).reduce((a, b) => a + b, 0);
+  const totalReactions = allArticles.map((article) => article.positive_reactions_count).reduce((a, b) => a + b, 0);
   const popularArticles = allArticles.sort(byScore).slice(0, 9);
-  const day = today();
+  const day = elaboratedDay();
   const title = `The 9 most popular Articles of today (${day})`;
   const description = `Another day, another list of popular tags (edition of ${day}).`;
   const content = readme`
 # ${title}
 ${description}
+
+Based on ${totalOfArticles} articles with ${totalComments} 💬 and ${totalReactions} 💕.
 
 ${popularArticles.map(toArticlePositionLine)} 
 ---
